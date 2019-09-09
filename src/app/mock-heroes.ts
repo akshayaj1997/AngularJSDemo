fcd200b7d0ce90dc2efd5f2a96390de57d3a2302
Akshaya Jonnalagadda <ajonnalagadda@ezesoft.com>
2019-09-09 16:51:17 +0530

    This is the intial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Strange',alias:'Dr.Stephen Strange',comics:'Marvel' },
+  { id: 12, name: 'Spiderman',alias:'Peter Parker',comics:'Marvel'},
+  { id: 13, name: 'Wolverine', alias:'Logan',comics:'Marvel' },
+  {id: 14,name: "Iron Man",alias: "Antony Edward Stark",comics: "Marvel"}
+];
