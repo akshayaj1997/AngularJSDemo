56fe2c1d474d2ce0dc67ca603a3d01703e744b1b
Akshaya Jonnalagadda <ajonnalagadda@ezesoft.com>
2019-09-09 17:21:25 +0530

    This is 2nd commit

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,5 +4,8 @@ export const HEROES: Hero[] = [
   { id: 11, name: 'Dr Strange',alias:'Dr.Stephen Strange',comics:'Marvel' },
   { id: 12, name: 'Spiderman',alias:'Peter Parker',comics:'Marvel'},
   { id: 13, name: 'Wolverine', alias:'Logan',comics:'Marvel' },
-  {id: 14,name: "Iron Man",alias: "Antony Edward Stark",comics: "Marvel"}
+  {id: 14,name: "Iron Man",alias: "Antony Edward Stark",comics: "Marvel"},
+  {id: 15,name: "Superman",alias: "Clark Kent",comics: "DC"},
+  {id: 16,name: "Batman",alias: "Bruce Wayne",comics: "DC"},
+  {id: 17,name: "Flash",alias: "Barry Allen",comics: "DC"}
 ];
